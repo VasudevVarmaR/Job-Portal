0343336c1050ae05b76cba509c6d3c6968e700c2
VasudevVarmaR <76203157+VasudevVarmaR@users.noreply.github.com>
2021-10-10 19:55:37 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -2,51 +2,42 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs' 
-import WriteStoryScreen from './screens/WriteStoryScreen'
-import ReadStoryScreen from './screens/ReadStoryScreen'
+import EnterDetails from './screens/EnterDetails'
+import ReadDetails from './screens/ReadDetails'
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default class App extends React.Component {
   render(){
     return(
+      <SafeAreaProvider>
       <AppContainer />
+      </SafeAreaProvider>
     );
   }
 }
 
 const TabNavigator = createBottomTabNavigator({
-  WriteStory: WriteStoryScreen,
-  ReadStory: ReadStoryScreen
+  EnterDetails: EnterDetails,
+  ReadDetails: ReadDetails
 },
 {
   defaultNavigationOptions: ({navigation})=>({
     tabBarIcon: ()=>{
-      
-      routeName
-    //  const routeName = navigation.routeName;
-     const routeName = navigation.state.routeName;
-    //  const routeName = navigation.state;
-    //  const routeName = state.routeName.navigation;
-       
-    
-      if(routeName === "WriteStory"){
+      const routeName = navigation.state.routeName;
+      console.log(routeName)
+      if(routeName === "EnterDetails"){
         return(
           <Image
-         // source={require("assets/write.png")}
-         // source={require("./write.png")}
-           source={require("./assets/write.png")}
-         // source={require("write.png")}
+          source={require("./assets/job1.png")}
           style={{width:40, height:40}}
         />
         )
         
       }
-      else if(routeName === "ReadStory"){
+      else if(routeName === "ReadDetails"){
         return(
           <Image
-          // source={require("./read.png")}
-          // source={require("read.png")}
-          // source={require("./assets")}
-           source={require("./assets/read.png")}
+          source={require("./assets/job2.webp")}
           style={{width:40, height:40}}
         />)
         
@@ -58,11 +49,5 @@ const TabNavigator = createBottomTabNavigator({
 
 const AppContainer = createAppContainer(TabNavigator);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
